1be9cea2c868f8b6c1894627de4f7e06a3383faa
Holger Krupp <email@holgerkrupp.de>
2025-02-04 07:46:19 +0100

    Initial Commit

---
+++ ClassCleaner/ClassCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassCleanerApp.swift
+//  ClassCleaner
+//
+//  Created by Holger Krupp on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ClassCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
